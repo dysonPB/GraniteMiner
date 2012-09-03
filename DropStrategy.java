9dd7f81a76f5ed94ad73dbac4128fa2e8421b3b7
dysonPB <kristo_ojasaar@hotmail.com>
2012-09-03 12:15:59 +0300

    Update DropStrategy.java

--- DropStrategy.java
+++ DropStrategy.java
@@ -2,6 +2,7 @@ package GraniteMiner;
 
 import org.powerbot.concurrent.strategy.Strategy;
 import org.powerbot.game.api.methods.tab.Inventory;
+import org.powerbot.game.api.util.Time;
 import org.powerbot.game.api.wrappers.node.Item;
 
 import GraniteMiner.Container.cons;
@@ -11,15 +12,17 @@ public class DropStrategy extends Strategy implements Runnable {
 	@Override
 	public void run() {
 		Item Granite = Inventory.getItem(cons.GRANITE_ID);
-		
-		if (Inventory.isFull()){
-			Granite.getWidgetChild().interact("Drop");}
+
+		if (Inventory.isFull()) {
+			Granite.getWidgetChild().interact("Drop");
+			Time.sleep(100, 200);
 		}
-		 @Override
-         public boolean validate() {
-                 return Inventory.getCount() == 28;
-		
 	}
 
+	@Override
+	public boolean validate() {
+		return Inventory.isFull();
+
+	}
 
 }
